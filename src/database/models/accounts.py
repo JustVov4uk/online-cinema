dc56c8bdec5f37ff8b7d46a5df93333118baac56
JustVov4uk <volodabudzan4@gmail.com>
2026-06-28 18:18:16 +0200

    feat: add user model

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from enum import StrEnum
 
-from sqlalchemy import Enum
+from sqlalchemy import Enum, Integer, String, Boolean, DateTime, func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database.models import Base
@@ -23,3 +24,19 @@ class UserGroup(Base):
         unique=True,
         nullable=False,
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    hashed_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
+    )
+    group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id"), nullable=False)
