f87efd9105c672f7940bd2e4d94e948db1443966
JustVov4uk <volodabudzan4@gmail.com>
2026-08-01 13:17:49 +0200

    feat: add role permission dependencies

--- src/api/dependencies/auth.py
+++ src/api/dependencies/auth.py
@@ -6,7 +6,7 @@ from jwt import InvalidTokenError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.security import decode_token
-from src.database.models.accounts import User
+from src.database.models.accounts import User, UserGroup, UserGroupEnum
 from src.database.session import get_database
 
 bearer_scheme = HTTPBearer()
@@ -47,3 +47,32 @@ async def get_current_user(
         )
 
     return user
+
+async def require_admin(
+    current_user: Annotated[User, Depends(get_current_user)],
+    session: Annotated[AsyncSession, Depends(get_database)],
+) -> User:
+    user_group = await session.get(UserGroup, current_user.group_id)
+
+    if user_group is None or user_group.name != UserGroupEnum.ADMIN:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin permissions required."
+        )
+    return current_user
+
+async def require_moderator_or_admin(
+    current_user: Annotated[User, Depends(get_current_user)],
+    session: Annotated[AsyncSession, Depends(get_database)],
+) -> User:
+    user_group = await session.get(UserGroup, current_user.group_id)
+
+    allowed_groups = {UserGroupEnum.MODERATOR, UserGroupEnum.ADMIN}
+
+    if user_group is None or user_group.name not in allowed_groups:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Moderator or admin permissions required."
+        )
+
+    return current_user
